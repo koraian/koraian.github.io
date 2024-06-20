144a1df774ab6800ce2ff7bd9d6c1c9828229148
singilStay <159459994+singilStay@users.noreply.github.com>
2024-06-20 17:38:28 +0900

    Update Footer.tsx

--- src/routes/Feed/Footer.tsx
+++ src/routes/Feed/Footer.tsx
@@ -14,7 +14,7 @@ const Footer: React.FC<Props> = ({ className }) => {
   return (
     <StyledWrapper className={className}>
       <a
-        href={`https://github.com/${CONFIG.profile.github}`}
+        href={`https://koraia.org`}
         target="_blank"
         rel="noreferrer"
       >
